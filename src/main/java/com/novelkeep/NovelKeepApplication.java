6123dc6b589b7c13c2874d8ecc443a1a844f128e
021skyfall <021skyfall@gmail.com>
2026-07-26 18:52:36 +0900

    chore: 노벨킵 기본 골조와 Docker 실행 환경 구성

---
+++ src/main/java/com/novelkeep/NovelKeepApplication.java
@@ -0,0 +1,12 @@
+package com.novelkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelKeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelKeepApplication.class, args);
+    }
+}
